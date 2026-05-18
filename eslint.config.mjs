c7a57d79fb90201ba4703612d65b0b7f74a021fb
andy <andy@apmicro.co.uk>
2026-05-18 13:18:20 +0100

    add some globals to stop eslint complaining

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,18 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        console: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        Date: "readonly",
+        Math: "readonly",
+        parseInt: "readonly",
+      },
+    },
     rules: {
       curly: ["error", "all"],
     },
